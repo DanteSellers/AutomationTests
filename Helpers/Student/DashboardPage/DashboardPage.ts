6617cf6506b1685b0cd74201d1da302d5b53b21a
William Delarosa <william.delarosa@gmetrix.com>
2022-12-18 06:32:46 -0600

    first commit

---
+++ Helpers/Student/DashboardPage/DashboardPage.ts
@@ -0,0 +1,5 @@
+import { Page } from "@playwright/test";
+
+export default class DashboardPage {
+  constructor(public page: Page) {}
+}
---
+++ Tests/Student/LoginTests/StudentLoginSuccessTest.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@playwright/test";
+import LoginPage from "../../../Helpers/Student/LoginPage/LoginPage";
+
+// This test checks that when entering invalid login credentials,
+// the login both fails and displays the following message:
+// "Invalid username and/or pasword."
+
+test("Test Login With Correct Credentials", async ({ page }) => {});
